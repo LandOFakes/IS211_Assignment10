f20a0ab425951a1fd14358c81ba16c057c40e32c
LandOFakes <P.jbrown198@gmail.com>
2025-04-27 14:17:28 -0400

    Update query_pets.py

--- query_pets.py
+++ query_pets.py
@@ -1,17 +1,12 @@
 import sqlite3
-
 conn = sqlite3.connect('pets.db')  # No spaces before conn
 cursor = conn.cursor()
-
 while True:
     person_id = int(input("Enter person ID (-1 to quit): "))
     if person_id == -1:
         break
-
     cursor.execute("SELECT ... FROM person WHERE id = ?", (person_id,))
     # ... (Fetch person data and print)
-
     cursor.execute("SELECT ... FROM pet JOIN person_pet ... WHERE person_id = ?", (person_id,))
     # ... (Fetch pets data and print)
-
 conn.close()
